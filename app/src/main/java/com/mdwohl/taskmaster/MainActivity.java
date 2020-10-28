1890a1b7b8b55d004661a0c3d6fad27ebc3aa3de
mdwohl <mdwohl@gmail.com>
2020-10-28 15:02:45 -0700

    Finished feature tasks for Lab 27

--- app/src/main/java/com/mdwohl/taskmaster/MainActivity.java
+++ app/src/main/java/com/mdwohl/taskmaster/MainActivity.java
@@ -3,12 +3,22 @@ package com.mdwohl.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView username = findViewById(R.id.displayUsernameMain);
+        username.setText(preferences.getString("username", "Go to Settings to create username"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,5 +61,17 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("task","Walk Dog");
             MainActivity.this.startActivity(intent);
         });
+
+        Button settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener((view) ->{
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            MainActivity.this.startActivity(intent);
+        });
+
+//        Intent intent = getIntent();
+//        String username = intent.getStringExtra("username");
+//        intent.putExtra("task", username);
+//        TextView currentTaskText = findViewById(R.id.usernameTasks);
+//        currentTaskText.setText(username + "'s tasks");
     }
 }
--- app/src/main/java/com/mdwohl/taskmaster/Settings.java
+++ app/src/main/java/com/mdwohl/taskmaster/Settings.java
@@ -3,8 +3,12 @@ package com.mdwohl.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,16 +19,21 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Button saveUsername = findViewById(R.id.saveUsernameButton);
+
         saveUsername.setOnClickListener((view) -> {
-            TextView username = findViewById(R.id.usernameField);
+            TextView username = findViewById(R.id.enterUsernameField);
             Context context = getApplicationContext();
 
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor preferenceEditor = preferences.edit();
+            preferenceEditor.putString("username", username.getText().toString() + "'s tasks:");
+            preferenceEditor.apply();
 
-            String usernameString = username.getText().toString();
-            String text = usernameString;
+            Intent goHome = new Intent(Settings.this, MainActivity.class);
+            Settings.this.startActivity(goHome);
             int duration = Toast.LENGTH_SHORT;
 
-            Toast toast = Toast.makeText(context, text, duration);
+            Toast toast = Toast.makeText(context, "Username Saved", duration);
             toast.show();
 
         });
